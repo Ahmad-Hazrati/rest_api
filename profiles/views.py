69bde3dfbeb2befdc667c810ec7dd935e8cfd914
Ahmad-Hazrati <hazrati.ahmad@gmail.com>
2024-03-07 14:09:16 +0100

    Install restframework and create ProfileList view

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,12 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Profile
 
-# Create your views here.
+
+class ProfileList(APIView):
+    """
+    List all profiles, or create a new profile.
+    """
+    def get(self, request):
+        profiles = Profile.objects.all()
+        return Response(profiles)
